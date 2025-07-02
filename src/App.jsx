7298d7b256ed2dff4f518f2112e432ff801a71fa
EOcapital <oron@eocapitalgroup.com>
2025-07-02 17:06:19 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 function App() {
-  return (
-    <div className="p-8 text-center">
-      <h1 className="text-4xl font-bold mb-4">EOC Lead Tracker</h1>
-      <p className="text-lg">Welcome to the final working version of your app!</p>
-    </div>
-  );
+  return <h1>EO Capital Lead Tracker is working!</h1>
 }
 
-export default App;
+export default App
